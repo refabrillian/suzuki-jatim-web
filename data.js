0a91742635ac6e9739844bf52c12f5cdaf62bfe2
refaaaa <refabrillianananda@gmail.com>
2026-02-12 16:35:49 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,25 @@
+const daftarMobil = [
+    {
+        nama: "FRONX",
+        harga: "261.500.000",
+        gambar: "image/FRONX-1,FRONX-2,FRONX-3/SGXsnowwhite(flip)_1755157326466.webp",
+        tag: "Best Seller",
+        linkDetail: "detail-fronx.html"
+    },
+    {
+        nama: "All New Ertiga Hybrid",
+        harga: "255.500.000",
+        gambar: "image/newertiga/allnewertigahybrid.jpg",
+        tag: "Promo",
+        linkWA: "https://wa.me/628123456789?text=Halo, saya tertarik dengan Ertiga",
+        linkDetail: "detail-newertiga.html"
+    },
+    {
+        nama: "Jimny 5-Door",
+        harga: "460.000.000",
+        gambar: "image/jimny5.jpg",
+        tag: "Exclusive",
+        linkWA: "https://wa.me/628123456789?text=Halo, saya tertarik dengan Jimny",
+        linkDetail: "detail-jimny5.html"
+    }
+];
