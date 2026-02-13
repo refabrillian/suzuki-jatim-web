e5d21c7c3c059c60af703afe868a04c67fbff8d9
refaaaa <refabrillianananda@gmail.com>
2026-02-13 13:13:26 +0700

    tambah data mobil data.js

--- data.js
+++ data.js
@@ -1,25 +1,61 @@
 const daftarMobil = [
     {
         nama: "FRONX",
-        harga: "261.500.000",
+        harga: "269.400.000",
         gambar: "image/FRONX-1,FRONX-2,FRONX-3/SGXsnowwhite(flip)_1755157326466.webp",
         tag: "Best Seller",
         linkDetail: "detail-fronx.html"
     },
     {
-        nama: "All New Ertiga Hybrid",
-        harga: "255.500.000",
+        nama: "All New Ertiga",
+        harga: "277.500.000",
         gambar: "image/newertiga/allnewertigahybrid.jpg",
         tag: "Promo",
-        linkWA: "https://wa.me/628123456789?text=Halo, saya tertarik dengan Ertiga",
+        linkWA: "https://wa.me/6285716547760?text=Halo, saya tertarik dengan Ertiga",
         linkDetail: "detail-newertiga.html"
     },
     {
         nama: "Jimny 5-Door",
-        harga: "460.000.000",
+        harga: "515.000.000",
         gambar: "image/jimny5.jpg",
         tag: "Exclusive",
-        linkWA: "https://wa.me/628123456789?text=Halo, saya tertarik dengan Jimny",
+        linkWA: "https://wa.me/6285716547760?text=Halo, saya tertarik dengan Jimny",
         linkDetail: "detail-jimny5.html"
-    }
+    },
+
+    {
+        nama: "XL7",
+        harga: "279.000.000",
+        gambar: "image/xl7/main.jpg",
+        tag: "Exclusive",
+        linkWA: "https://wa.me/6285716547760?text=Halo, saya tertarik dengan XL7",
+        linkDetail: "detail-xl7.html"
+    },
+
+    {
+        nama: "Grand Vitara",
+        harga: "409.750.000",
+        gambar: "image/grandvitara/23_GrandVitara2025Front4582BAPassengerSide-SpendidSilver2_1763634860877.webp",
+        tag: "Exclusive",
+        linkWA: "https://wa.me/6285716547760?text=Halo, saya tertarik dengan Grand Vitara",
+        linkDetail: "detail-grandvitara.html"
+    },
+
+    {
+        nama: "S-Presso",
+        harga: "188.750.000",
+        gambar: "image/s-presso/ORANGE_1723101525534.webp",
+        tag: "Exclusive",
+        linkWA: "https://wa.me/6285716547760?text=Halo, saya tertarik dengan S-Presso",
+        linkDetail: "detail-s-presso.html"
+    },
+
+    {
+        nama: "New Carry",
+        harga: "193.500.000",
+        gambar: "image/carry/PrimeGraphiteGreyresize_1756091078424.webp",
+        tag: "Exclusive",
+        linkWA: "https://wa.me/6285716547760?text=Halo, saya tertarik dengan Carry",
+        linkDetail: "detail-carry.html"
+    },
 ];
